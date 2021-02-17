3adf9e1be04871dbf7bbfc5aa47ed50f7b424083
bpcam <bpcampassi@gmail.com>
2021-02-17 00:12:54 -0500

    Added Bet Class and BankTrans class

--- Casino.Data/BankTransaction.cs
+++ Casino.Data/BankTransaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace Casino.Data
 {
     public class BankTransaction
     {
+        [Key]
+        public int BankTransId { get; set; }
+
+        [ForeignKey(nameof(Player))]
+        public int PlayerID { get; set; }
+        public virtual Player Player { get; set; }
+
+        [Required]
+        public DateTimeOffset DateTimeOfTrans { get; set; }
+
+        public double BankTransAmount { get; set; } //positive for deposit, negative for withdraw
+
+
     }
 }
--- Casino.Data/Bet.cs
+++ Casino.Data/Bet.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,29 @@ namespace Casino.Data
 {
     public class Bet
     {
-        public string FirstName { get; set; }
+        [Key]
+        public int BetId { get; set; }
+
+        [ForeignKey(nameof(Player))]
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; }
+
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+
+        [Required]
+        public double BetAmount { get; set; }
+
+        public bool PlayerWonGame { get; set; }
+
+        public double PayoutAmount { get; set; } //Positive for win, Negative for loss
+
+        public DateTimeOffset DateTimeOfBet { get; set; }
+
+
+
+
+
     }
 }
