dce18658613e2233c5c079f0e11157b945e41f52
bpcam <bpcampassi@gmail.com>
2021-02-17 22:41:27 -0500

    GameSim class and BetService class

---
+++ Casino.Models/GameSimulation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Models
+{
+   public class GameSimulation
+    {
+        public bool PlayerWonGame(int gameId)
+        {
+            //logic for game based on gameId
+            //if game1 (run this method)
+            //else (run this method)
+            //return true or false;
+            return true;
+        }
+    }
+}
---
+++ Casino.Services/BetService.cs
@@ -0,0 +1,48 @@
+﻿using Casino.Data;
+using Casino.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Services
+{
+    public class BetService
+    {
+        private GameSimulation _gameSim = new GameSimulation();
+        private readonly Guid _playerId;  //Parse from currently logged in player
+        private readonly Player _currentPlayerId;
+        public BetService(Player player)//try this way or see other version just below
+        {
+           // _currentPlayerId = player.PlayerId;
+        }
+        public BetService(Guid userId)
+        {
+
+        }
+    public bool CreateBet(BetCreate model)
+    {
+
+        var entity = new Bet()
+        {
+            // PlayerId = _playerId; //if we go this route need to add Guid to Bet class
+            //PlayerId = _currentPlayerId;
+            //PlayerId = Player.PlayerId;
+            PlayerId = model.PlayerId,
+            GameId = model.GameId,
+            BetAmount = model.BetAmount,
+            PlayerWonGame = _gameSim.PlayerWonGame(model.GameId),
+
+        };
+        using (var ctx = new ApplicationDbContext())
+        {
+
+            ctx.Bets.Add(entity);
+            return ctx.SaveChanges() != 0;
+        }
+
+    }
+}
+
+    }
