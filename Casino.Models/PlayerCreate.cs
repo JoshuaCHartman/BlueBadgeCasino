90fea0ec6ead7bd0ffb0bb425b522d9aa3b704a6
bpcam <bpcampassi@gmail.com>
2021-03-02 20:32:17 -0500

    Just Cheking out

--- Casino.Models/PlayerCreate.cs
+++ Casino.Models/PlayerCreate.cs
@@ -44,7 +44,7 @@ namespace Casino.Models {
         public DateTimeOffset AccountCreated { get; set; }
 
 
-        public bool IsActive { get; set; }// = true;
+        public bool IsActive { get; set; } = true;
 
         //public bool IsActive { get; set; }
 
