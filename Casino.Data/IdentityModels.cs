24dc2b602f8c8ba9dd58315e9fb9412c314a3316
JoshuaCHartman <joshuachartman@gmail.com>
2021-03-08 23:09:45 -0500

    updated connection string to default

--- Casino.Data/IdentityModels.cs
+++ Casino.Data/IdentityModels.cs
@@ -26,7 +26,7 @@ namespace Casino.Data
 
           // Using test db PrivateConn.config connection string
 
-          : base("Brian2", throwIfV1Schema: false)
+          : base("DefaultConnection", throwIfV1Schema: false)
 
         {
         }
